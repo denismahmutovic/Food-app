6b8842840841e8a5dc6e73e2f0108a7277bc6339
denismahmutovic <djdeny95@gmail.com>
2023-02-06 22:45:25 +0100

    start

---
+++ screens/HomeScreen.js
@@ -0,0 +1,34 @@
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
+export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView>
+      <Text className={"text-red-500"}></Text>
+
+      <View className={"flex-row"}>
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className={`h-7 w-7 bg-gray-300 p-4 rounded-full`}
+        />
+
+        <View>
+          <Text className={`font-bold text-gray-400 text-xs`}>
+            Deliver Now!
+          </Text>
+          <Text className={`font-bold text-xl`}>Current Location</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
