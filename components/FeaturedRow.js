472f6ba1384d0824894f1ee9da479578b89e855c
denismahmutovic <djdeny95@gmail.com>
2023-02-07 22:09:20 +0100

    Card

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { ArrowRightIcon } from "react-native-heroicons/outline";
 
-export default function FeaturedRow({ title, description }) {
+export default function FeaturedRow({ id, title, description }) {
   return (
     <View>
-      <Text>FeaturedRow</Text>
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color={"#00CCBB"} />
+      </View>
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+      />
     </View>
   );
 }
