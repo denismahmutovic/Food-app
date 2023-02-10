36d62e39435cd71f5fefb448a3921e5c19524b5a
denismahmutovic <djdeny95@gmail.com>
2023-02-10 22:25:48 +0100

    work

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,8 +1,30 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
+import { urlFor } from "../sanity";
+import {
+  ArrowLeftIcon,
+  ChevronRightIcon,
+  StarIcon,
+} from "react-native-heroicons/solid";
 
 export default function RestaurantScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   const {
     params: {
       id,
@@ -18,8 +40,41 @@ export default function RestaurantScreen() {
     },
   } = useRoute();
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Image
+          source={{
+            uri: imgUrl,
+          }}
+          className={`w-full h-56 bg-gray p-4`}
+        />
+        <TouchableOpacity
+          onPress={navigation.goBack}
+          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
+        >
+          <ArrowLeftIcon size={20} color="#00CCBB" />
+        </TouchableOpacity>
+        <View className="bg-white">
+          <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flex-row space-x2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <StarIcon color={"green"} opacity={0.5} size={22} />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-500">{rating}</Text>.{genre}
+                </Text>
+              </View>
+
+              <View className="flex-row items-center space-x-1">
+                {/* <LocarionMarkeIcon color={"green"} opacity={0.5} size={22} /> */}
+
+                <Text className="text-gray-500">Nearby . {address}</Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
